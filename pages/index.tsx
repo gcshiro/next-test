d9ca4035edf8f17a5963510ad3f138986029471c
gcshiro <s.shirota@gemcook.com>
2021-03-18 21:30:08 +0900

    #

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ const IndexPage = () => (
       <meta property="og:image" content="" />
       <meta property="og:site_name" content="github.com/" />
     </Head>
-    <h1>Hello POCKET OGPテスト 1</h1>
+    <h1>POCKET OGPテスト 2</h1>
     <p>
       <a href="http://getpocket.com/edit?url=https://gcshiro.github.io/next-test/&title=OGPテストです。これはタイトルとは違います。" rel="nofollow" target="_blank">
         Pocket
