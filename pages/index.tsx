6041a8bfbac227cf5ff0aa36a09b3f959641d232
gcshiro <s.shirota@gemcook.com>
2021-03-18 16:52:52 +0900

    init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,12 @@
-import Link from 'next/link'
 import Layout from '../components/Layout'
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>Hello Next.js 👋</h1>
     <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
+      <a href="http://getpocket.com/edit?url=https://webdesign-trends.net/entry/3632&title=オリジナルのシェアボタンを作ろう！各種SNSのボタン設置用URLまとめ" rel="nofollow" target="_blank">
+        <i className="fab fa-get-pocket" />
+      </a>
     </p>
   </Layout>
 )
