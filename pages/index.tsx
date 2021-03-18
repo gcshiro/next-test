88b9b3a0230521a835624882f3e2ec8fca176886
gcshiro <s.shirota@gemcook.com>
2021-03-18 17:04:31 +0900

    #

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,8 @@ const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>Hello Next.js 👋</h1>
     <p>
-      <a href="http://getpocket.com/edit?url=https://webdesign-trends.net/entry/3632&title=オリジナルのシェアボタンを作ろう！各種SNSのボタン設置用URLまとめ" rel="nofollow" target="_blank">
-        <i className="fab fa-get-pocket" />
+      <a href="http://getpocket.com/edit?url=https://gcshiro.github.io/next-test/&title=OGPテスト" rel="nofollow" target="_blank">
+        Pocket
       </a>
     </p>
   </Layout>
